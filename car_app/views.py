f5d92d46316ae104a2c8ab7a48e98d6663c55a37
Nahidjc <nahidjc80@gmail.com>
2021-10-28 15:54:50 +0600

    viewset created

--- car_app/views.py
+++ car_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .models import Services
+from .serializers import ServiceSerializer
 # Create your views here.
+
+class ServiceViewSet(viewsets.ModelViewSet):
+    serializer_class = ServiceSerializer
+    def get_queryset(self):
+        return Services.objects.all()
