0a150960a080bd5e4cfb9487dd05e3b11044c4c2
Nahidjc <nahidjc80@gmail.com>
2021-10-29 20:09:13 +0600

    post api created

--- car_app/urls.py
+++ car_app/urls.py
@@ -2,5 +2,7 @@
 from django.urls import path
 from car_app import views
 urlpatterns = [
-    path('services/', views.ServiceViewSet.as_view(), name='services'),
+    path('services/',
+         views.ServiceViewSet.as_view(), name='services'),
+    path('add-service/', views.create_service, name='create-service'),
 ]
--- car_app/views.py
+++ car_app/views.py
@@ -1,3 +1,4 @@
+from django.db.models import query
 from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Services
@@ -5,17 +6,30 @@ from .serializers import ServiceSerializer
 from rest_framework.response import Response
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework import permissions, serializers, status
+from rest_framework.decorators import api_view
+from rest_framework import generics
+
 # Create your views here.
 
 
-class ServiceViewSet(ListAPIView):
+class ServiceViewSet(generics.ListCreateAPIView):
+    queryset = Services.objects.all()
     serializer_class = ServiceSerializer
+    permission_classes = (permissions.AllowAny,)
+
     # def get_queryset(self):
     #     return Services.objects.all()
-    permission_classes = (permissions.AllowAny,)
-    queryset = Services.objects.all()
+    # permission_classes = (permissions.AllowAny,)
+    # queryset = Services.objects.all()
 
     # def list(self, request):
     #     queryset = Services.objects.all()
     #     serializer = ServiceSerializer(queryset, many=True)
     #     return Response(serializer.data)
+
+
+@api_view(['POST'])
+def create_service(request):
+    if request.method == 'POST':
+        return Response({"message": "Got some data!", "data": request.data})
+    return Response({"message": "Hello, world!"})
